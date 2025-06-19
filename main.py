80c269c2cb338e5f484b6a065ab23f969db9db6c
defrome <iskoblov555@gmail.com>
2025-06-19 17:47:24 +0300

    first

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-from fastapi import FastAPI
+
+
+from fastapi import FastAPI, Header, HTTPException
+
+app = FastAPI()
+
+API_KEY = 'd11caa373e4345afb76143753251906'
+
+async def verified_api(api_key: str = Header()):
+    if api_key != API_KEY:
+        raise HTTPException(status_code=403, detail='Invalid API_KEY')
