6fb2e69520719937f4b9927b53ada2953333869b
defrome <iskoblov555@gmail.com>
2025-06-19 18:07:48 +0300

    first

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from operator import truediv
 import os
 import uvicorn
+import httpx
 from fastapi import FastAPI, Header, HTTPException
 
 app = FastAPI()
@@ -12,6 +13,19 @@ async def verified_api(api_key: str):
     if api_key != API_KEY:
         raise HTTPException(status_code=403, detail='Invalid API_KEY')
     return True
+
+@app.get('/current_weather')
+async def get_current_weather(city: str):
+    async with httpx.AsyncClient() as client:
+        response = await client.get(
+            f"http://api.weatherapi.com/v1/current.json?key=d11caa373e4345afb76143753251906&q={city}&aqi=no"
+        )
+        data = response.json()
+        return {
+            "city": city,
+            "temp": data['current']['temp_c']
+        }
+
 
 print(API_KEY)
 
