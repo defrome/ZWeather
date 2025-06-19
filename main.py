ec5ad856b11d76515419e91451cdd98cb73e747a
defrome <iskoblov555@gmail.com>
2025-06-19 17:53:20 +0300

    first

--- main.py
+++ main.py
@@ -7,14 +7,11 @@ app = FastAPI()
 
 API_KEY = 'd11caa373e4345afb76143753251906'
 
-async def verified_api(api_key: str = Header()):
+@app.get('/verified_api')
+async def verified_api(api_key: str):
     if api_key != API_KEY:
         raise HTTPException(status_code=403, detail='Invalid API_KEY')
     return True
 
-@app.get('/verified_api')
-async def verified_api_get():
-    await verified_api()
-
 if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=8000)
+    uvicorn.run(app, host="localhost", port=3000)
