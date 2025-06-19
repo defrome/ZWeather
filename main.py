8b357ae1a77e0a188e9b9aa259834703d42e2ea6
defrome <iskoblov555@gmail.com>
2025-06-19 17:48:58 +0300

    first

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+from operator import truediv
 
 from fastapi import FastAPI, Header, HTTPException
 
@@ -9,3 +9,8 @@ API_KEY = 'd11caa373e4345afb76143753251906'
 async def verified_api(api_key: str = Header()):
     if api_key != API_KEY:
         raise HTTPException(status_code=403, detail='Invalid API_KEY')
+    return True
+
+@app.get('/verified_api')
+async def verified_api_get():
+    await verified_api()
